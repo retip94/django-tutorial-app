360c49b000af3eedb9f73a647916b92c26fd2945
retip94 <38405004+retip94@users.noreply.github.com>
2019-12-03 14:51:52 +0100

    changes in admin panel

--- first_app/polls/tests.py
+++ first_app/polls/tests.py
@@ -107,3 +107,25 @@ class QuestionDetailViewTests(TestCase):
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
 
+
+class QuestionResultsViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The results of the question with pub_date in the future returns 404
+        """
+        future_question = create_question(question_text='Future question',days=30)
+        url = reverse('polls:results', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The results of the question with pub_date in the past displays the question's text
+        """
+        past_question = create_question(question_text='Past question',days=-30)
+        url=reverse('polls:results',args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
+
--- first_app/polls/views.py
+++ first_app/polls/views.py
@@ -13,7 +13,6 @@ from django.utils import timezone
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
@@ -24,12 +23,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
     def get_queryset(self):
         """
         Excludes any question, which isn't published yet
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
@@ -37,6 +36,12 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+    def get_queryset(self):
+        """
+        Excludes any question which is not published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
